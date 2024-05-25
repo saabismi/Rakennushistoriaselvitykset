66e0bc9f235ba293857628431c7386dc445f944b
saabismi <vilho.syvahuoko@gmail.com>
2024-05-25 19:02:46 +0300

    Lisäilty kohteita

---
+++ map.js
@@ -0,0 +1,34 @@
+var map = L.map('map').setView([60.185, 24.83], 12);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+function onEachFeature(feature, layer) {
+    if (feature.properties && feature.properties.name && feature.properties.address && feature.properties.rhs && feature.properties.rhs_archive) {
+        layer.bindPopup(
+            '<strong>' + feature.properties.name + '</strong><br>' +
+            feature.properties.address + '<br>' +
+            '<a href="' + feature.properties.rhs + '" target="_blank">Rakennushistoriaselvitys</a>' + '<br>' +
+            '<a href="https://web.archive.org/' + feature.properties.rhs + '" target="_blank">Rakennushistoriaselvitys (arkistoitu)</a>'
+        );
+    }
+}
+
+L.geoJSON(rakennushistoria, {onEachFeature: onEachFeature}).addTo(map); // Add GeoJSON layer
+
+/*
+// A button for getting coordinates visible by clicking on the map
+function enableAdminmode() {
+    var popup = L.popup();
+
+    function onMapClick(e) {
+        popup
+            .setLatLng(e.latlng)
+            .setContent("Tämä pisteen WGS84-koordinaatit: " + e.latlng.toString())
+            .openOn(map);
+    }
+    map.on('click', onMapClick);
+}
+*/
---
+++ rakennushistoria.js
@@ -0,0 +1,148 @@
+let rakennushistoria = 
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            24.825003, 60.181594
+          ]
+        },
+        "properties": {
+          "address": "Vuorimiehentie 1",
+          "name": "Puunjalostusosasto/Puu1",
+          "rhs": "https://static.espoo.fi/cdn/ff/PEpSybTmnn39vSoPm7enlNJkYVHpyqg58yHENXxZX_s/1678457249/public/2023-03/220209%20Kivimies%20Vuorimiehentie%201%20rakennushistoriaselvitys%202022.pdf",
+          "rhs_archive": "https://web.archive.org/web/20240515003310/https://static.espoo.fi/cdn/ff/PEpSybTmnn39vSoPm7enlNJkYVHpyqg58yHENXxZX_s/1678457249/public/2023-03/220209%20Kivimies%20Vuorimiehentie%201%20rakennushistoriaselvitys%202022.pdf"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            24.828115, 60.18272
+          ]
+        },
+        "properties": {
+          "address": "Vuorimiehentie 5",
+          "name": "VTT päärakennus",
+          "rhs": "https://static.espoo.fi/cdn/ff/2ANXhnrqbXAMYsCbB4IQ-Hw7mL-po7GqhQg-StZRgxY/1678456783/public/2023-03/220209%20Kivimies%20Vuorimiehentie%205%20rakennushistoriaselvitys%202022.pdf",
+          "rhs_archive": "https://web.archive.org/web/20240515003255/https://static.espoo.fi/cdn/ff/2ANXhnrqbXAMYsCbB4IQ-Hw7mL-po7GqhQg-StZRgxY/1678456783/public/2023-03/220209%20Kivimies%20Vuorimiehentie%205%20rakennushistoriaselvitys%202022.pdf"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            24.795113, 60.178084
+          ]
+        },
+        "properties": {
+          "address": "Ahertajantie 6",
+          "name": "Tapiolan Teollisuustalo",
+          "rhs": "https://static.espoo.fi/cdn/ff/uWJ5ICDBlOPDqR115PQ1fvy6JyIE1fVFLyV6wNmvp7I/1687186941/public/2023-06/213501%20Ahertajankulma%20II%20rakennushistoriaselvitys%202023.pdf"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            24.805176, 60.192631
+          ]
+        },
+        "properties": {
+          "address": "Metsäpirtintie 19",
+          "name": "SOS-lapsikylä Pohjois-Tapiola",
+          "rhs": "https://static.espoo.fi/cdn/ff/bHlncFEKWvql2Dz-b-GVdimKFjP6PZPV814bcj3tR3o/1617871916/public/2021-04/Rakennushistoriaselvitys%202016%20Ei%20saavutettava.PDF"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            24.776037, 60.16288
+          ]
+        },
+        "properties": {
+          "address": "Ukkohauentie 11-13",
+          "name": "Länsiviitta (Haukilahden kauppakeskus)",
+          "rhs": "https://static.espoo.fi/cdn/ff/7-1sG8GlpKS4202vtEZydGdOopKBD55wL71MMRfqozs/1707810714/public/2024-02/230970%20Haukilahden%20keskusta%20RHS%20Kauppakeskus%20L%C3%A4nsiviitta%202023.pdf"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            24.824451, 60.18794
+          ]
+        },
+        "properties": {
+          "address": "Puumiehenkuja 5",
+          "name": "Otaniemen Konelaboratorio",
+          "rhs": "https://static.espoo.fi/cdn/ff/i2e4cFzpCuh5jJl-tdRSDrA94MyPf4lbpIpDAyFLl9E/1632227583/public/2021-09/Rakennushistoriaselvitys%20Puumiehenkuja%205%20ei%20saavutettava.pdf"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            24.774567, 60.163179
+          ]
+        },
+        "properties": {
+          "address": "Ukkohauentie 9",
+          "name": "Haukilahden (vanha) ostoskeskus",
+          "rhs": "https://static.espoo.fi/cdn/ff/lIjPscHghQWkTMah6JCI-xTQCKDO392Uf7sg58_bTm8/1707810713/public/2024-02/230970%20Haukilahden%20keskusta%20RHS%20Haukilahden%20ostoskeskus%202023.pdf"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            25.103352, 60.402742
+          ]
+        },
+        "properties": {
+          "address": "Kauppakaari 1",
+          "name": "Anttilan talo / Pohjolan liiketalo",
+          "rhs": "https://arkbyroo.fi/projektit/kauppakaari-1"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            24.763023, 60.228533
+          ]
+        },
+        "properties": {
+          "address": "Karakallio",
+          "name": "Karakallion ostoskeskus",
+          "rhs": "https://arkbyroo.fi/projektit/karakallion-ostoskeskus"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            24.601436, 60.185836
+          ]
+        },
+        "properties": {
+          "address": "Lasikuja 2",
+          "name": "Oy Slev Ab Kauklahti",
+          "rhs": "https://static.espoo.fi/cdn/ff/QmX20_kdHHs9-v7ykv9DhuSnVsE5mdPrT7Hebha-40U/1619612876/public/2021-04/Rakennushistoriaselvitys%20Oy%20Slev%20Ab%20Kauklahti%202018%20Ei%20saavutettava.pdf"
+        }
+      }
+    ]
+  }
